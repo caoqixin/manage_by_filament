ef29581abac1074f7c899ae9d6408b4288769c39
xin <caoqixin7@gmail.com>
2023-04-25 14:08:42 +0200

    部署优化

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +12,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
@@ -60,4 +61,14 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    protected $accessList = [
+        'caoqixin7@gmail.com',
+        'bailingli0623@icloud.com'
+    ];
+
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, '@mediatech-xin.com') || in_array($this->email, $this->accessList);
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <a href="/admin"
+                   class="text-white bg-blue-700
+                   hover:bg-blue-800 focus:ring-4 focus:ring-blue-300
+                   font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600
+                   dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">进入后台</a>
                 {{--                搜索表单--}}
                 {{--                购物车--}}
             </div>
